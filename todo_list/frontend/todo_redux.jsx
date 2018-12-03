ed62504489d421100942a3f6e3b516b6389ed658
guanw88 <guanw88@gmail.com>
2018-12-03 13:54:18 -0800

    Set up React skeleton

---
+++ todo_list/frontend/todo_redux.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import Redux
+// function Root() {
+//   return (
+//     "<div>React is working!</div>"; 
+//   )
+// } 
+  
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<div>React is working</div>, document.getElementById("root"));
+});
