2ec7ebb9ff7221b72ba68c1bb115f05dd957edf1
guanw88 <guanw88@gmail.com>
2018-12-03 14:23:03 -0800

    Set up rootReducer and store

---
+++ todo_list/frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import todosReducer from "./todos_reducer";
+
+const rootReducer = combineReducers(
+  {
+    todo: todosReducer
+  }
+);
+
+export default rootReducer;
--- todo_list/frontend/todo_redux.jsx
+++ todo_list/frontend/todo_redux.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import configureStore from "./store/store"
+
 // import Redux
 // function Root() {
 //   return (
 //     "<div>React is working!</div>"; 
 //   )
 // } 
+
+window.store = configureStore();
   
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<div>React is working</div>, document.getElementById("root"));
+  configureStore();
+  ReactDOM.render(<h1>Todos App</h1>, document.getElementById("root"));
 });
