c0d2f0ead3cea04d01ae9bdc00c2ac809dfb416e
guanw88 <guanw88@gmail.com>
2018-12-04 15:18:03 -0800

    Add form to add to do items

--- todo_list/frontend/components/todos/todo_list.jsx
+++ todo_list/frontend/components/todos/todo_list.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import TodoListItem from "./todo_list_item";
+import TodoForm from "./todo_form";
 
 
-export default ({todos}) => {
+export default ({todos, addTodo}) => {
   const todoTitles = todos.map(todo => {
     // return <li key={todo.id}>{todo.title}</li>;
     return <TodoListItem key={todo.id} todo={todo}/>
@@ -11,6 +12,7 @@ export default ({todos}) => {
   return (
     <>
       <h3>Todo List goes here!</h3>
+      <TodoForm addTodo={addTodo}/>
       <ul>
         {todoTitles}
       </ul>
--- todo_list/frontend/components/todos/todo_list_item.jsx
+++ todo_list/frontend/components/todos/todo_list_item.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default ({todo}) => {
-  console.log(todo);
   return (
     <li>{todo.title}</li>
   );
